27320d0656cda90b58a464ad44f5280c56ec794c
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-26 17:42:46 +0200

    add: AppTitle.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <script>
+  import AppTitle from "@/components/AppTitle.vue"
 
+  export default{
+    name: "Home",
+    components: {
+      AppTitle,
+    },
+    data(){
+      return{
+
+      }
+    }
+  }
 </script>
 
 <template>
-
+  <div class="pg-wrapper">
+    <AppTitle/>
+  </div>
 </template>
 
-<style>
-
+<style scoped>
+  .pg-wrapper{
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data(){
+    return{
+      titleMsg: "La mia prima app con Vite!",
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{titleMsg}}</h1>
+</template>
+
+<style scoped>
+  h1{
+    color: white;
+    transition: all .5s;
+    text-align: center;
+  }
+
+  h1:hover{
+    color: wheat;
+    text-shadow: 0 0 25px;
+    cursor: default;
+  }
+</style>
